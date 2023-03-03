42a3feebe6e2caa6e5705baac4a606982356eeda
David Sheldrick <d.j.sheldrick@gmail.com>
2023-03-03 10:50:29 +0000

    fix docs ordering

---
+++ scripts/order-docs.ts
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import { existsSync, readdirSync, readFileSync, renameSync, statSync, writeFileSync } from 'fs'
+import { join } from 'path'
+import { pathToFileURL } from 'url'
+
+type Ordering = Array<string | [string, Ordering] | [string]>
+
+function orderDocs(packageName: string) {
+	const orderingFile = join('packages', packageName, 'docs-ordering.json')
+	if (!existsSync(orderingFile)) {
+		return
+	}
+	console.log('Ordering docs for ' + packageName)
+
+	const ordering: Ordering = JSON.parse(readFileSync(orderingFile, 'utf8'))
+
+	function applyOrdering(ordering: Ordering, filesDir: string) {
+		for (const entry of ordering) {
+			if (Array.isArray(entry)) {
+				const [dirName, subOrdering] = entry
+				const dirPath = join(filesDir, dirName)
+				if (!statSync(dirPath).isDirectory()) {
+					throw new Error(`Dir ${dirPath} does not exist`)
+				}
+				writeFileSync(join(dirPath, '_category_.yml'), `position: ${ordering.indexOf(entry)}`)
+				if (subOrdering) {
+					applyOrdering(subOrdering, dirPath)
+				}
+			} else {
+				const filePath = join(filesDir, entry + '.md')
+				if (!existsSync(filePath)) {
+					throw new Error(`File ${filePath} does not exist`)
+				}
+				const newFilePath = join(filesDir, `${ordering.indexOf(entry)}.${entry}.md`)
+				console.log(`Renaming ${entry} to ${ordering.indexOf(entry)}.md`)
+				renameSync(filePath, newFilePath)
+			}
+		}
+	}
+
+	applyOrdering(ordering, `docs/docs/API/${packageName.replace(/-/g, '_')}`)
+}
+
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+	// module was called directly
+	for (const packageName of readdirSync('packages')) {
+		orderDocs(packageName)
+	}
+}
