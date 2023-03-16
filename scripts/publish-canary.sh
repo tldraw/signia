0384dc5e81a8e282ef5988ad65cfb49bcacebb78
David Sheldrick <d.j.sheldrick@gmail.com>
2023-03-16 14:54:05 +0000

    set auth token in publish script (#63)

--- scripts/publish-canary.sh
+++ scripts/publish-canary.sh
@@ -13,5 +13,7 @@ fi
 
 project_root=$(git rev-parse --show-toplevel)
 
+npm config set _authToken $NPM_TOKEN
+
 echo "publishing canary version"
 pnpm -r exec -- tsx $project_root/scripts/publish-canary-version.ts $type
